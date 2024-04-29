ca7c3cf8e1b69adedd5ea4f29224425fe029dccf
ysgyeong00 <144661643+ysgyeong00@users.noreply.github.com>
2024-04-29 14:04:30 +0900

    Create ChatServer.java

---
+++ ChatServer.java
@@ -0,0 +1,25 @@
+package project;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ChatServer {
+    private final ServerSocket serverSocket;
+
+    public ChatServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void runServer() {
+        try {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
